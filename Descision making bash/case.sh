4be0afd30f58c4104497e04c556eeb7a54750294
Anusha S Reddy <4q4947@gmail.com>
2026-09-26 12:25:12 +0530

    Create case.sh for profession identification
    Add a case statement to display profession based on name.

---
+++ Descision making bash/case.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Name="Sahil"
+
+case "$Name" in
+    "Gulfam") echo "Profession : Software Engineer" ;;
+    "Sujal") echo "Profession : Web Developer" ;;
+    "Sahil") echo "Profession : Technical Content Writer Linux" ;; #ends the pattern block
+esac #Ends the case statement
